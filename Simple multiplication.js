3a28bc9eb1c3a0fda4a07ed9054e0d133e8557b5
imonoho <imonoho@gmail.com>
2020-08-05 14:51:26 -0500

    //You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.     //If it is a square, return its area. If it is a rectangle, return its perimeter.
    //Note: for the purposes of this kata you will
    // assume that it is a square if its length and width are equal, otherwise it is a rectangle.

--- Simple multiplication.js
+++ Simple multiplication.js
@@ -2,6 +2,9 @@
 
 //This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
 
-function simpleMultiplication(number) {
-    return number % 2 === 0 ? number * 8 : number * 9;
+function score  (current, level) {
+ return level === 'easy' ? current + 2 :
+            level === 'medium' ? current + 4 : current + 8 ;
+
 }
+console.log(score(400, 'hard'));
