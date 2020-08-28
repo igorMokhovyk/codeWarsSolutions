48076ae665e2cd7fb865ac5ab34e5fe3a2503f11
imonoho <imonoho@gmail.com>
2020-08-28 18:00:18 -0500

    // https://www.codewars.com/kata/53dc54212259ed3d4f00071c/train/javascript
    // Sum Array
    // Write a method sum (sum_array in python, sumarray in julia, SumArray in C#) that takes an array of numbers and returns the sum of the numbers. These may be integers or decimals for Ruby and any instance of Num for Haskell. The numbers can also be negative. If the array does not contain any numbers then you should return 0.
    //
    // Examples
    // numbers = [1, 5.2, 4, 0, -1]
    // puts sum(numbers)
    // 9.2
    // Assumptions
    // You can assume that you are only given numbers.
    // You cannot assume the size of the array.
    // You can assume that you do get an array and if the array is empty,

---
+++ Sum Arrays. 8 kyu.js
@@ -0,0 +1,21 @@
+// https://www.codewars.com/kata/53dc54212259ed3d4f00071c/train/javascript
+
+// Sum Array
+// Write a method sum (sum_array in python, sumarray in julia, SumArray in C#) that takes an array of numbers and returns the sum of the numbers. These may be integers or decimals for Ruby and any instance of Num for Haskell. The numbers can also be negative. If the array does not contain any numbers then you should return 0.
+//
+// Examples
+// numbers = [1, 5.2, 4, 0, -1]
+// puts sum(numbers)
+// 9.2
+// Assumptions
+// You can assume that you are only given numbers.
+// You cannot assume the size of the array.
+// You can assume that you do get an array and if the array is empty,
+
+function sum(numbers){
+    let sum = 0;
+    for(let i = 0; i < numbers.length; i++){
+        sum += numbers[i];
+    }
+    return sum;
+}
