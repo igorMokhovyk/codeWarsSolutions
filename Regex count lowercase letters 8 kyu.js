54c925b9ffe769403d22d5ff969cc77b7cea5e54
imonoho <imonoho@gmail.com>
2020-09-13 19:08:50 -0500

    // https://www.codewars.com/kata/56a946cd7bd95ccab2000055/train/javascript
     // Your task is simply to count the total number of lowercase letters in a string.
    //
    // Examples
    // lowercaseCount("abc"); ===> 3
    //
    // lowercaseCount("abcABC123"); ===> 3
    //
    // lowercaseCount("abcABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~"); ===> 3
    //
    // lowercaseCount(""); ===> 0;
    //
    // lowercaseCount("ABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~"); ===> 0
    //
    // lowercaseCount("abcdefghijklmnopqrstuvwxyz"); ===> 26

---
+++ Regex count lowercase letters 8 kyu.js
@@ -0,0 +1,23 @@
+// https://www.codewars.com/kata/56a946cd7bd95ccab2000055/train/javascript
+
+ // Your task is simply to count the total number of lowercase letters in a string.
+//
+// Examples
+// lowercaseCount("abc"); ===> 3
+//
+// lowercaseCount("abcABC123"); ===> 3
+//
+// lowercaseCount("abcABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~"); ===> 3
+//
+// lowercaseCount(""); ===> 0;
+//
+// lowercaseCount("ABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~"); ===> 0
+//
+// lowercaseCount("abcdefghijklmnopqrstuvwxyz"); ===> 26
+
+
+
+function lowercaseCount(str){
+    let patern = /[a-z]/g;
+    return str.match(patern) ? str.match(patern).length : 0;
+}
