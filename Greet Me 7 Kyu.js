a88eb6e8f9997ebac2ef2624101d4735bbb69629
imonoho <imonoho@gmail.com>
2020-12-03 18:29:47 -0600

    // https://www.codewars.com/kata/535474308bb336c9980006f2/train/javascript
    // Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
    //
    // Example:
    //
    // "riley" --> "Hello Riley!"
    // "JACK"  --> "Hello Jack!"

---
+++ Greet Me 7 Kyu.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/535474308bb336c9980006f2/train/javascript
+
+// Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
+//
+// Example:
+//
+// "riley" --> "Hello Riley!"
+// "JACK"  --> "Hello Jack!"
+
+const greet = function(name) {
+    return `Hello ${name.toLowerCase().charAt(0).toUpperCase() + name.toLowerCase().slice(1)}!`
+};
