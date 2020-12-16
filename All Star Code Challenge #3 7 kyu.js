3f28f91977ce8c88a660efafb4eaf59a156d8db0
imonoho <imonoho@gmail.com>
2020-12-15 18:59:38 -0600

    //https://www.codewars.com/kata/58640340b3a675d9a70000b9/train/javascript
    //This Kata is intended as a small challenge for my students
    //
    // Create a function, called removeVowels (or remove_vowels), that takes a string argument and returns that same string with all vowels removed (vowels are "a", "e", "i", "o", "u").
    //
    // removeVowels("drake") // => "drk"
    // removeVowels("aeiou") // => ""

---
+++ All Star Code Challenge #3 7 kyu.js
@@ -0,0 +1,14 @@
+//https://www.codewars.com/kata/58640340b3a675d9a70000b9/train/javascript
+
+
+//This Kata is intended as a small challenge for my students
+//
+// Create a function, called removeVowels (or remove_vowels), that takes a string argument and returns that same string with all vowels removed (vowels are "a", "e", "i", "o", "u").
+//
+// removeVowels("drake") // => "drk"
+// removeVowels("aeiou") // => ""
+
+
+const removeVowels = function(str){
+    return str.replace(/[aeiou]/gi, '');
+}
