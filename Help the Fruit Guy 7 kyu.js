cf4cb68b7d060fbf6b1b727ebfa02e07c7291a56
imonoho <imonoho@gmail.com>
2020-12-16 20:53:56 -0600

    // https://www.codewars.com/kata/557af4c6169ac832300000ba/train/javascript
    // Our fruit guy has a bag of fruit (represented as an array of strings) where some fruits are rotten. He wants to replace all the rotten pieces of fruit with fresh ones. For example, given ["apple","rottenBanana","apple"] the replaced array should be ["apple","banana","apple"]. Your task is to implement a method that accepts an array of strings containing fruits should returns an array of strings where all the rotten fruits are replaced by good ones.
    //
    // Notes
    // If the array is null/nil/None or empty you should return empty array ([]).
    // The rotten fruit name will be in this camelcase (rottenFruit).
    // The returned array should be in lowercase.

---
+++ Help the Fruit Guy 7 kyu.js
@@ -0,0 +1,27 @@
+// https://www.codewars.com/kata/557af4c6169ac832300000ba/train/javascript
+
+// Our fruit guy has a bag of fruit (represented as an array of strings) where some fruits are rotten. He wants to replace all the rotten pieces of fruit with fresh ones. For example, given ["apple","rottenBanana","apple"] the replaced array should be ["apple","banana","apple"]. Your task is to implement a method that accepts an array of strings containing fruits should returns an array of strings where all the rotten fruits are replaced by good ones.
+//
+// Notes
+// If the array is null/nil/None or empty you should return empty array ([]).
+// The rotten fruit name will be in this camelcase (rottenFruit).
+// The returned array should be in lowercase.
+
+
+function removeRotten(bagOfFruits){
+    let ready = [];
+    if (bagOfFruits === null){
+        return [];
+    } else {
+        for (let i = 0; i < bagOfFruits.length; i++) {
+            if (!bagOfFruits[i].includes('rotten')){
+                ready.push(bagOfFruits[i]);
+            }
+            if (bagOfFruits[i].includes('rotten')){
+                ready.push(bagOfFruits[i].toLowerCase().replace("rotten", '')) ;
+            }
+        }
+    }
+
+    return ready;
+}
