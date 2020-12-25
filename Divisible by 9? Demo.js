0939de62a9a8eca9da2383a1b8b7fe8757059f14
imonoho <imonoho@gmail.com>
2020-12-24 22:25:00 -0600

    // Given a non-negative integer number represented as a string, which can be arbitrary large. Detect whether it is divisible by 9? // // e.g // // 0 -> true // 7 -> false // 9 -> true // 18 -> true // 19 -> false // 777777777777777777777777777777777777777777777 -> true // Tips: // // All input strings in the tests are valid non-negative integer numbers, you don't have to check that. // You'd better not use the arbitrary-precision data types from chosen programming languages, if any, to keep the kata fun :) // For Java, avoid BigInteger or BigDecimal. // For Javascript, avoid BigInt. // For Python3, since int has unbounded range, so avoid converting the whole input as a single int directly. // etc ...

---
+++ Divisible by 9? Demo.js
@@ -0,0 +1,29 @@
+// Given a non-negative integer number represented as a string, which can be arbitrary large. Detect whether it is divisible by 9?
+//
+// e.g
+//
+// 0 -> true
+// 7 -> false
+// 9 -> true
+// 18 -> true
+// 19 -> false
+// 777777777777777777777777777777777777777777777 -> true
+// Tips:
+//
+// All input strings in the tests are valid non-negative integer numbers, you don't have to check that.
+// You'd better not use the arbitrary-precision data types from chosen programming languages, if any, to keep the kata fun :)
+// For Java, avoid BigInteger or BigDecimal.
+// For Javascript, avoid BigInt.
+// For Python3, since int has unbounded range, so avoid converting the whole input as a single int directly.
+// etc ...
+
+// codewars.com/kata/5f7b1a82be51af00105c82bd/train/javascript
+
+
+function divBy9(ns){
+
+    return (ns.split('').reduce((a, b) => +a + +b)) % 9 === 0
+}
+
+console.log(divBy9("777777777777777777777777777777777777777777777"))
+
